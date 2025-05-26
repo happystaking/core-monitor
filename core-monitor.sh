0ac83304e47a4ff9f16045f792ce81b48406a7c0
Leon • HAPPY <info@happystaking.io>
2025-05-26 09:49:52 +0200

    Rewrite logic and drop cncli dependency in favor of cardano-cli

--- core-monitor.sh
+++ core-monitor.sh
@@ -19,7 +19,7 @@ localCoreKeysPath=
 # Script variables
 network=${1:-mainnet}
 localIsForging=false
-dependencies=(cardano-cli cncli curl jq)
+dependencies=(cardano-cli curl jq)
 
 # Dependency checks
 for binary in ${dependencies[@]}
@@ -32,9 +32,12 @@ do
 done
 
 # Sanity checks
-if [[ ! -s "$localCoreKeysPath"/kes.skey ||  ! -s "$localCoreKeysPath"/node.cert ||  ! -s "$localCoreKeysPath"/vrf.skey ||  ! -w "$localCoreKeysPath" ]];
+if [[ ( ! -s "${localCoreKeysPath}/kes.skey" && ! -s "${localCoreKeysPath}/kes.skey.standby" ) ||
+      ( ! -s "${localCoreKeysPath}/node.cert" && ! -s "${localCoreKeysPath}/node.cert.standby" ) ||
+      ( ! -s "${localCoreKeysPath}/vrf.skey" && ! -s "${localCoreKeysPath}/vrf.skey.standby" ) ||
+      ( ! -w "${localCoreKeysPath}" ) ]];
 then
-    echo "Error: the files kes.skey, node.cert and vrf.skey must all be present in $localCoreKeysPath and this script must have write permissions on that directory."
+    echo "Error: the files kes.skey, node.cert and vrf.skey (or standby versions) must be present in $localCoreKeysPath and this script must have write permissions on that directory."
     exit 1;
 fi
 
@@ -53,8 +56,13 @@ function getBlockHeightDiff { # args: localBlockHeight remoteBlockHeight
     echo "$1 - $2" | bc
 }
 
-function getRemoteCncliState {
-    echo `cncli ping -h $remoteCoreAddress -p $remoteCorePort | jq -r .status`
+function getRemoteCoreState {
+    tip=$(cardano-cli ping -jtqc 1 -h $remoteCoreAddress -p $remoteCorePort 2>/dev/null)
+    if [ "$?" == "0" ]; then echo "$tip" | jq -r ".tip[0]"; else echo "error"; fi
+}
+
+function getRemoteCoreOnTip { #args: remoteCoreState localBlockHeight
+    if [[ "$1" != "error" && `echo "${1}" | jq -r ".blockNo"` -ge $(( $2 - $blockHeightDiffThreshold )) ]]; then echo "true"; else echo "false"; fi
 }
 
 function getConnectivityState {
@@ -63,7 +71,7 @@ function getConnectivityState {
 }
 
 function getTunnelState {
-    if [ "$tunnelCheckPerform" == "false" ]; then echo "ok1" && return; fi
+    if [ "$tunnelCheckPerform" == "false" ]; then echo "ok" && return; fi
     for hostIP in "${tunnelCheckIPs[@]}"
     do
         ping -c 2 -w 4 $hostIP &> /dev/null
@@ -94,53 +102,45 @@ function deactivateLocalCore {
 while :
 do
     localBlockHeight=$(getLocalBlockHeight)
-    remoteCncliState=$(getRemoteCncliState)
     remoteBlockHeight=$(getRemoteBlockHeight)
     blockHeightDiff=$(getBlockHeightDiff $localBlockHeight $remoteBlockHeight)
 
-    echo "stage: 1; local height: $localBlockHeight; remote height: $remoteBlockHeight; diff: $blockHeightDiff"
+    echo "stage: 1; local height: $localBlockHeight; remote height: $remoteBlockHeight; diff: $blockHeightDiff; threshold: $blockHeightDiffThreshold"
 
-    # The difference between the local block height and block height reported by remote Prometheus is above the threshold (remote core is lagging behind).
+    # The difference between the local block height and block height reported by remote Prometheus is above the threshold.
+    # The next step will use cardano-cli to confirm if the remote core is really lagging behind.
     if [[ $blockHeightDiff -gt $blockHeightDiffThreshold ]];
     then
         tunnelState=$(getTunnelState)
-        connectivityState=$(getConnectivityState)
+        remoteCoreState=$(getRemoteCoreState)
         remoteCheckState=$(getRemoteCheckState)
+        remoteCoreOnTip=$(getRemoteCoreOnTip "$remoteCoreState" $localBlockHeight)
+        connectivityState=$(getConnectivityState)
 
-        echo "stage: 2; threshold: $blockHeightDiffThreshold; diff: $blockHeightDiff; remote cncli: $remoteCncliState; connectivity: $connectivityState; tunnel: $tunnelState; check state: $remoteCheckState"
+        echo "stage: 2; connectivity: $connectivityState; cardano-cli remote core on tip: $remoteCoreOnTip; tunnel: $tunnelState; check port: $remoteCheckState"
 
-        # Activate the local core only under the prerequisite that: we have connectivity, we're not forging blocks already and:
-        #  1) the remote block height is not reported (0), tunnel is ok, but cncli (through tunnel) reports an error, or
+        # Activate the local core only under the condition that: we have connectivity, we're not forging blocks already, remote core is not on tip and:
+        #  1) the remote block height is not reported (0) and the tunnel is ok, or
         #  2) the remote block height is not reported (0) and the remote check port is not open (host down), or
-        #  3) the remote block height is reported (not 0), but is above the set threshold. ATTN: in this case you have two producers running and forking could potentially happen!
-        if [[ "$connectivityState" == "ok" && "$localIsForging" == false && ( ( "$tunnelState" == "ok" && "$remoteCncliState" == "error" && "$remoteBlockHeight" == "0" ) ||  ( "$remoteBlockHeight" == "0" && "$remoteCheckState" != "open" ) || "$remoteBlockHeight" != "0" )]];
+        #  3) the remote block height is reported (not 0), but is above the set threshold (checked in previous step). ATTN: in this case you have two producers running and forking could potentially happen!
+        if [[ "$connectivityState" == "ok" && "$localIsForging" == false && "$remoteCoreOnTip" == "false" && ( "$tunnelState" == "ok" || "$remoteCheckState" != "open" ) ]];
         then
-            echo "stage: 3; activating: local core; remote cncli: $remoteCncliState; forging: $localIsForging; connectivity: $connectivityState; diff: $blockHeightDiff; remote height: $remoteBlockHeight; check state: $remoteCheckState"
-
             localIsForging=true
             activateLocalCore
+            echo "stage: 3; activated local core; local forging: $localIsForging; cardano-cli remote core on tip: $remoteCoreOnTip; tunnel state: $tunnelState; check port: $remoteCheckState"
         # Just logging that the local BP is currently running.
         elif [[ "$localIsForging" == true ]];
         then
-            echo "stage: 3; running: local core; remote cncli: $remoteCncliState; forging: $localIsForging; connectivity: $connectivityState; diff: $blockHeightDiff; remote height: $remoteBlockHeight; check state: $remoteCheckState"
-        # Error reported by cncli because we have no internet connectivity.
-        elif [[ "$connectivityState" == "error" ]];
-        then
-            echo "stage: 3; skipping: local core activation; remote cncli: $remoteCncliState; forging: $localIsForging; connectivity: $connectivityState; diff: $blockHeightDiff remote height: $remoteBlockHeight; check state: $remoteCheckState"
-        # Log that the local BP is now running because the remote host is down.
-        elif [[ "$remoteCheckState" != "open" && "$remoteBlockHeight" == "0" ]];
-        then
-            echo "stage: 3; running: local core; remote cncli: $remoteCncliState; forging: $localIsForging; connectivity: $connectivityState; diff: $blockHeightDiff; remote height: $remoteBlockHeight; check state: $remoteCheckState"
+            echo "stage: 3; forging on local core; local forging: $localIsForging; cardano-cli remote core on tip: $remoteCoreOnTip; tunnel state: $tunnelState; check port: $remoteCheckState"
         fi
     fi
 
-    # Disable the local core if the height diff is 0 or above, below threshold again or cncli reports success and we're forging blocks locally.
-    if [[ ( $blockHeightDiff -ge 0 && $blockHeightDiff -le $blockHeightDiffThreshold || "$remoteCncliState" == "ok" ) && "$localIsForging" == true ]];
+    # Disable the local core if the height diff is 0 or above, below threshold again or cardano-cli reports success and we're forging blocks locally.
+    if [[ ( $blockHeightDiff -ge 0 && $blockHeightDiff -le $blockHeightDiffThreshold || "$remoteCoreOnTip" == "true" ) && "$localIsForging" == true ]];
     then
-        echo "stage: 2; deactivated: local core; remote cncli: $remoteCncliState; forging: $localIsForging; connectivity: $connectivityState; diff: $blockHeightDiff; check state: $remoteCheckState"
-
         localIsForging=false
         deactivateLocalCore
+        echo "stage: 2; deactivated local core; local forging: $localIsForging; cardano-cli remote core on tip: $remoteCoreOnTip"
     fi
 
     sleep $secondsSleepMainLoop
